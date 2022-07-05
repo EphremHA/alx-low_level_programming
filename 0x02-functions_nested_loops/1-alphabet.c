2299afe1a6b68efcdb796771795f37d0fea261cb
EphremHA <ephrem.24@gmail.com>
2022-07-05 07:34:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints small case alphabet
+ * _putchar - Displays characters
+ *
+ * 
+ *
+ * 
+ */
+void print_alphabet(void)
+{
+	int s;
+
+	for (s = 97; s <= 122; ++s)
+	{
+		_putchar(s);
+	}
+	_putchar(10);
+}
