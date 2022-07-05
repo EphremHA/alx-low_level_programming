a30234940382e24e024de63a9b6a2a56c392577f
EphremHA <ephrem.24@gmail.com>
2022-07-05 12:02:40 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,14 @@
 /**
  * print_last_digit - last one
  * _putchar - Displays characters
+ * _abs - Absolute
  * @g: Variable
  *
- * Return: always g
+ * Return: always 0.
  */
 int print_last_digit(int g)
 {
-	g = g - ((g / 10) * 10);
-	return (g);
+	g  = g - ((g / 10) * 10);
+	_putchar(g);
+	return (0);
 }
