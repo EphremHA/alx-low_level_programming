3f68ed75dbddfc82a3ed8156956cf7f270f2b5f9
EphremHA <ephrem.24@gmail.com>
2022-07-05 06:20:52 -0700

    putc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,10 @@
  * Return: Always 0.
  *
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 int main (void)
 {
 	int s = 95;
@@ -15,24 +19,23 @@ int main (void)
 	while (i < 8)
 	{
 	_putchar(s);
-	++i;
-	
-	if ( s == 95)
+
+	if ( i == 0)
 		s = 112;
-	else if (s == 112)	
+	else if (i == 1)	
 		s = 117;
-	else if (s == 117 )
+	else if (i == 2 )
 		s = 116;
-	else if (s == 116)
+	else if (i == 3)
 		s = 99;
-	else if (s == 99 )
+	else if (i == 4 )
 		s = 104;
-	else if (s == 104)
+	else if (i == 5)
 		s = 97;
-	else if (s == 97 )
+	else if (i == 6 )
 		s = 114;
-	if (i == 7)
-		s = 10;
-	}	
-return (0);
+	++i;
+	}
+	_putchar(10);
+	return (0);
 }
