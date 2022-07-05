479a4007a1761b43f374dce32f4dd296f147571e
EphremHA <ephrem.24@gmail.com>
2022-07-05 08:31:40 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - prints small case alphabet 10x
+ * _putchar - Displays characters
+ * @c - Variable of function
+ *
+ * Return: always b
+ */
+int _islower(int c)
+{
+	int b;
+
+	if (c >= 97 && c <= 122)
+		b = 1;
+	else
+		b = 0;
+
+	return (b);
+}
