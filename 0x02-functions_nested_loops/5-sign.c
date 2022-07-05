564d1e91c544c97eb28138b22d96641ed4708865
EphremHA <ephrem.24@gmail.com>
2022-07-05 10:20:58 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - checks alphabet
+ * _putchar - Displays characters
+ * @c:  Variable of function
+ *
+ * Return: always b
+ */
+int print_sign(int n)
+{
+	int b;
+
+	if (n != 0)
+	{
+		if (n > 0)
+		{
+			_putchar(43);
+			b =1;
+		}
+		if (n < 0)
+		{
+			_putchar(45);
+			b = -1;
+		}
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		b = 0;
+	}
+	return (b);
+}
