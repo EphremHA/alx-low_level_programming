06ad04d9611900140a4b1c56bde0ae49086bc43e
EphremHA <ephrem.24@gmail.com>
2022-07-06 05:32:39 -0700

    Jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,31 +5,19 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j, k, l, c;
 
 	for (i = 48; i <= 50; ++i)
 	{
 		if (i == 48 || i == 49)
 		{
-			for (j = 48; j <= 57; ++j)
-			{
-				for (k = 48; k <= 53; ++k)
-				{
-					for (l = 48; l <= 57; ++l)
-					{
-						_putchar(i);
-						_putchar(j);
-						_putchar(58);
-						_putchar(k);
-						_putchar(l);
-						_putchar(10);
-					}
-				}
-			}
+			c = 57;
 		}
-		if (i == 50)
+		else
 		{
-			for (j = 48; j <= 51; ++j)
+			c = 51;
+		}
+			for (j = 48; j <= c; ++j)
 			{
 				for (k = 48; k <= 53; ++k)
 				{
@@ -44,6 +32,5 @@ void jack_bauer(void)
 					}
 				}
 			}
-		}
 	}
 }
