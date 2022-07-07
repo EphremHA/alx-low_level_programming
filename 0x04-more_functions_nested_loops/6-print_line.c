364a81d03e8273911ff00b68a93348d63605dea7
EphremHA <ephrem.24@gmail.com>
2022-07-07 13:11:32 -0700

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - adds numbers
+ * _putchar - Displays characters
+ *
+ * Return: Void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		if (n <= 0)
+		{
+			_putchar(10);
+			break;
+		}
+		else
+			_putchar(95);
+	}
+	_putchar(10);
+}
