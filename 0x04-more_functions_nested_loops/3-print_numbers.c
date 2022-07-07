8ce2ac00a7e0fed0c266ed56a3986cb592c0f23e
EphremHA <ephrem.24@gmail.com>
2022-07-07 11:58:27 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - adds numbers
+ * _putchar - Displays characters
+ *
+ * Return: Void
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
