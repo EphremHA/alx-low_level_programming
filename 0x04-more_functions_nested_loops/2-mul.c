6d5e0fe48744020f14306105f359a3dc90ef0e6d
EphremHA <ephrem.24@gmail.com>
2022-07-07 11:23:57 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies
+ * _putchar - Displays characters
+ * @a:  Variable of function
+ * @b: Variable
+ *
+ * Return: always b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
