7b13ae5f51ec650fe58efb410af3d2209cc5c684
EphremHA <ephrem.24@gmail.com>
2022-07-09 04:59:43 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ *
+ */
+
+int main(void)
+{
+	int write(int filedes, const char *buf, unsigned int nbyte);
+	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
