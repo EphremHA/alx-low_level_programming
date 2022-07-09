341bd182b05c296a45834474f4f95ea5ebffb506
EphremHA <ephrem.24@gmail.com>
2022-07-09 05:01:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	int write(int filedes, const char *buf, unsigned int nbyte);
-	
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
