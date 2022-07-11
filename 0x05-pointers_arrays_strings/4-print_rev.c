42d44082dfa9383837a9d3c9f5ceef8fe4bf5e1c
EphremHA <ephrem.24@gmail.com>
2022-07-11 10:32:48 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,11 +14,16 @@ void print_rev(char *s)
 	int length = 0;
 
 	for (j = 1; *(s + j) != 0; ++j)
+	{
 		length = j;
+		if (length == 0)
+			goto f;
+	}
 	for (i = length; i >= 0; --i)
 	{
 		_putchar(*(s + i));
 	}
+f:
 	_putchar(10);
 }
 
