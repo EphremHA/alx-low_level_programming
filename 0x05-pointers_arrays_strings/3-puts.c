c4e0176a7f9103add437f04045f353d946a4e8d2
EphremHA <ephrem.24@gmail.com>
2022-07-11 09:09:11 -0700

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,15 +10,15 @@
 
 void _puts(char *str)
 {
-	char pr[1000000];
+	/*char pr[1000000];*/
 	int i;
 	int stop = 1;
 
 	for (i = 0; stop != 0; ++i)
 	{
-		pr[i] = *(str + i);
-		if (pr[i] != 0)
-			_putchar(pr[i]);
+		/*pr[i] = *(str + i);*/
+		if (*(str + i) != 0)
+			_putchar(*(str + i));
 		else
 			stop = 0;
 	}
