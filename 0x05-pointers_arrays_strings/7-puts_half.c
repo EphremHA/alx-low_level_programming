1640b0c8f3d081f18398ffb4c81de3343efe6440
EphremHA <ephrem.24@gmail.com>
2022-07-11 12:54:07 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen - String length counter
+ * @str: parameter
+ *
+ * Return: none
+ */
+
+void puts_half(char *str)
+{
+	int i, length = 0, n;
+
+	for (i = 0; *(str + i) != 0; ++i)
+		length = i;
+	length = i + 1;
+	if (length % 2 != 0)
+		n = (length - 1) / 2;
+	else
+		n = length;
+	for (i = n; *(str + i) != 0; ++i)
+		_putchar(*(str + i));
+	_putchar(10);
+
+}
