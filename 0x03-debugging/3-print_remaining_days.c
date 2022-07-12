c918517816077b3656042d596cb70a93cc6f5f8d
EphremHA <ephrem.24@gmail.com>
2022-07-12 08:11:04 -0700

    3-print_remaining_days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,8 +18,17 @@ void print_remaining_days(int month, int day, int year)
 		{
 			if (year % 400 == 0)
 			{
-				printf("Day of the year: %d\n", day);
-				printf("Remaining days: %d\n", 366 - day);
+				if (day >= 60)
+				{
+					++day;
+					printf("Day of the year: %d\n", day);
+					printf("Remaining days: %d\n", 366 - day);
+				}
+				else
+				{
+					printf("Day of the year: %d\n", day);
+					printf("Remaining days: %d\n", 366 - day);
+				}
 			}
 			else
 			{
@@ -29,8 +38,18 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			if (day >= 60)
+			{
+				++day;
+				printf("Day of the year: %d\n", day);
+				printf("Remaining days: %d\n", 366 - day);
+			}
+			else
+			{
+				printf("Day of the year: %d\n", day);
+				printf("Remaining days: %d\n", 366 - day);
+			}
+				
 		}
 	}
 	else
