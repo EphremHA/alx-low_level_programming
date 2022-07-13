5493d4490a6ba2d7ae48e47f0f8c1fa31e133f5b
EphremHA <ephrem.24@gmail.com>
2022-07-13 02:28:03 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,24 +14,20 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
-		else if (i == 1)
-		{
-			printf("%d ", i);
-		}
-		else if (i == 2)
-		{
+		else
 			printf("%d", i);
-		}
+		if (i < 100 && i >= 1)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
