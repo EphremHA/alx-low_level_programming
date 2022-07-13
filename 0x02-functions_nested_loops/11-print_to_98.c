fe5b5b9787abda061bef64209cf37f8c2efea7de
EphremHA <ephrem.24@gmail.com>
2022-07-13 01:43:09 -0700

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,19 +3,31 @@
 
 /**
  * print_to_98 - NUmber list
- * @n: Variable
+ * * @n: Variable
  *
  * Return: Void
  */
 void print_to_98(int n)
 {
-	int i;
+	int i = 0;
 
 	if (n <= 98)
 		for (i = n; i <= 98; ++i)
-			printf("%d, ", i);
-	else
-		for (i = n; i > 98; --i)
-			printf("%d, ", i);
+		{
+			if (i < 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("98");
+		}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; --i)
+		{
+			if (i > 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("98");
+		}
+	}
 	printf("\n");
 }
