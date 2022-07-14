8cb4e0cda6fc1a3f69b24e5f3590cd93090409fa
EphremHA <ephrem.24@gmail.com>
2022-07-14 14:42:30 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, c = 0;
 
-	for(i = 0; *(s1 + i) != 0; ++i)
+	for (i = 0; *(s1 + i) != 0; ++i)
 	{
 		c = *(s1 + i) - *(s2 + i);
 		if (c != 0)
 		{
 			return (c);
-			break;
 		}
 	}
 	return (c);
