f6380e517805c098c2db730348c7b840b3ed9710
EphremHA <ephrem.24@gmail.com>
2022-07-14 15:15:52 -0700

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of int
+ * @a: param
+ * @n: param
+ *
+ * Return: a
+ */
+
+void reverse_array(int *a, int n)
+{
+	int r[10000], i;
+
+	for (i = 0; i <= n - 1; ++i)
+		*(r + i) = *(a + (n - 1) - i);
+	for (i = 0 ; i <= n; ++i)
+		*(a + i) = *(r + i);
+}
