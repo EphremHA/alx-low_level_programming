22694d4728c3ae498eae8ff8ca7172872f2db721
EphremHA <ephrem.24@gmail.com>
2022-07-14 07:45:49 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - Concatenate
+ * @dest: destination parameter
+ * @src: source parameter
+ *
+ * Return: *dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j, dlen, slen, conc;
+
+	for (i = 0; *(dest + i) != 0; ++i)
+		dlen = i;
+	for (j = 0; *(src + j) != 0; ++j)
+		slen = j;
+	for (i = 0; *(src + i) != 0; ++i)
+		*(dest + dlen + 1 + i) = *(src + i);
+	conc = dlen + slen;
+	*(dest + conc + 2) = 0;
+	return (dest);
+}
