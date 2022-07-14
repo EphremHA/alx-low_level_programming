9eb0e892ab346360bbd2dd1e461aa7c2dc4772d6
EphremHA <ephrem.24@gmail.com>
2022-07-14 11:54:57 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, dlen;
+	int i = 0, dlen = 0, slen = 0;
 
-	for (dlen = 0 ; *(dest + dlen) != '\0'; ++dlen)
-		;
-	for (i = 0; i <= n - 1; ++i)
-		*(dest + dlen + i) = *(src + i);
-	for (dlen = 0 ; *(dest + dlen) != '\0'; ++dlen)
-		;
-	*(dest + dlen) = '\0';
+	while (*(dest + dlen))
+		++dlen;
+	while (*(src + slen))
+		++slen;
+	if (n <= slen)
+		for (i = 0 ;i <= n - 1; ++i)
+			*(dest + dlen + i) = *(src + i);
+	else if (n > slen)
+		for (i = 0 ; *(src + i) != 0; ++i)
+			*(dest + dlen + i) = *(src + i);
+	*(dest + dlen + i) = '\0';
 	return (dest);
 }
