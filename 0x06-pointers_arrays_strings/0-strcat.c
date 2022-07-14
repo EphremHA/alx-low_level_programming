316f7f1f47e102671c628637a1622d2aa3b25074
EphremHA <ephrem.24@gmail.com>
2022-07-14 08:11:42 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,11 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j, dlen, slen, conc = 0;
 
-	for (i = 0; *(dest + i) != 0; ++i)
+	for (i = 0; *(dest + i) != '\0'; ++i)
 		dlen = i;
-	for (j = 0; *(src + j) != 0; ++j)
+	for (j = 0; *(src + j) != '\0'; ++j)
 		slen = j;
-	for (i = 0; *(src + i) != 0; ++i)
+	for (i = 0; *(src + i) != '\0'; ++i)
 		/**(dest + dlen + 1 + i) = *(src + i);*/
 		dest[dlen + 1 + i] = src[i];
 	conc = dlen + slen;
