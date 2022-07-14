0505970cd74f6c55c4a65362a39b2fc9aaa77337
EphremHA <ephrem.24@gmail.com>
2022-07-14 12:07:01 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Concatenate
+ * @dest: destination parameter
+ * @src: source parameter
+ * @n: byte to bo appended
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, dlen = 0, slen = 0;
+
+	for (dlen = 0; *(dest + dlen) != '\0'; ++dlen)
+		;
+	for (slen = 0; *(src + slen) != '\0'; ++slen)
+		;
+	if (n <= slen)
+		for (i = 0 ; i <= n - 1; ++i)
+			*(dest + i) = *(src + i);
+	else if (n > slen)
+		for (i = 0 ; *(src + i) != 0; ++i)
+			*(dest + i) = *(src + i);
+	*(dest + dlen + i) = '\0';
+	return (dest);
+}
