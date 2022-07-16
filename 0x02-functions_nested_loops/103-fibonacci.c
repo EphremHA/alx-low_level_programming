310a912d1205463f4b7627d62657500e6c358761
EphremHA <ephrem.24@gmail.com>
2022-07-16 09:30:20 -0700

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Sum of even Fibonacci series
+ *
+ * Return: o
+ */
+
+int main(void)
+{
+	long int i, sum, fn = 1, sn = 2, esum = 0;
+
+	for (i = 1; i <= 48; ++i)
+	{
+		sum = fn + sn;
+		if (sum <= 4000000 && (sum % 2 == 0))
+			esum += sum;
+		fn = sn;
+		sn = sum;
+	}
+	printf("%ld\n", esum);
+	return (0);
+}
