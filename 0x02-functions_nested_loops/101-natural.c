2c4d4888dc1d0eb7dfd556554ddeca1422fde425
EphremHA <ephrem.24@gmail.com>
2022-07-16 08:49:11 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,18 @@
 
 int main(void)
 {
-	int i, sum;
+	int i, sum = 0;
 
-	for (i = 0; (3 * i) < 1024; ++i)
-		sum += (3 * i);
-	for (i = 0; (5 * i) < 1024; ++i)
-		sum += (5 * i);
+	for (i = 1; (3 * i) < 1024; ++i)
+	{
+		if (((3 * i) % 15) != 0)
+			sum += 3 * i;
+		if (((3 * i) % 15) == 0)
+			sum += 3 * i;
+	}
+	for (i = 1; (5 * i) < 1024; ++i)
+		if (((5 * i) % 15) != 0)
+			sum += 5 * i;
 	printf("%d\n", sum);
 	return (0);
 }
