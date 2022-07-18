a0b46d509f5158c55337dce718e342f47a78a1e6
EphremHA <ephrem.24@gmail.com>
2022-07-18 05:11:56 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
 /**
  * *_strchr - searches value of c in s
  * @s: string to be searched
- * @c: source parameter
+ * @c: search character
  *
  * Return: mem. loc. of value of c in s or NULL depending on the search
  */
@@ -14,11 +14,7 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; i < (int) sizeof(s); i++)
-	{
 		if (*(s + i) == c)
-		{
 			return (&s[i]);
-		}
-	}
 	return (NULL);
 }
