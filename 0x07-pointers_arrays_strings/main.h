220545de85c1c598331e47d1fa9fd903449a1a73
EphremHA <ephrem.24@gmail.com>
2022-07-18 10:10:29 -0700

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
 #endif /* MAIN_H */
