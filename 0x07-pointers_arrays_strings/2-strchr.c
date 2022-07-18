4bed3a58d0d0f8c5eec0ed17d128a1020bdca727
EphremHA <ephrem.24@gmail.com>
2022-07-18 07:18:37 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,11 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; i < (int) sizeof(s); i++)
+	{
 		if (*(s + i) == c)
 			return (s + i);
+		if (s[i] == '\0')
+			return (NULL);
+	}
 	return (NULL);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 #include <stddef.h>
+unsigned int _strspn(char *s, char *accept);
 #endif /* MAIN_H */
