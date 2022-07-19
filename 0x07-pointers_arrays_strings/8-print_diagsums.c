4089aa834ad9ca8e6b2503745c0df1d7eb7116cb
EphremHA <ephrem.24@gmail.com>
2022-07-19 09:42:41 -0700

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,22 +11,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, usum, dsum, t;
+	long int i, j, usum = 0, dsum = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if(i == j)
-			{
-				t = a[i][j];
-				usum += t;
-			}
-			if (j == size - i)
-			{
-				/*dsum += a[i][j];*/
-			}
+			if (i == j)
+				usum += a[(i * size) + j];
+			if (i == (size - 1) - j && j == (size - 1) - i)
+				dsum += a[(i * size) + j];
 		}
 	}
-	printf("%d, %d\n", usum, dsum);
+	printf("%ld, %ld\n", usum, dsum);
 }
