9c8d19dab09d5b600318de747d9b4eff552c36c9
EphremHA <ephrem.24@gmail.com>
2022-07-20 08:07:15 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string one character at a time
+ * @s: string parameter
+ *
+ * Return: none
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(&s[1]);
+	}
+}
