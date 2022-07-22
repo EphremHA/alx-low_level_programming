37517ac35c6b5106c05db6fb7eb11922e8b5d0fd
EphremHA <ephrem.24@gmail.com>
2022-07-22 08:02:23 -0700

    argc[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: ardument count
+ * @argv: string vector
+ *
+ * Return: none
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[0]);
+	return (argc);
+}
