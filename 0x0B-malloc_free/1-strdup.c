60f34e1f41284b450d0fe44f31ce608972b4f14b
EphremHA <ephrem.24@gmail.com>
2022-07-25 13:58:16 -0700

    str_cpy

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,8 +18,8 @@ char *_strdup(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	p = malloc(i + 1);
-	if (str == NULL)
-		return (0);
+	if (p == NULL || str == NULL)
+		exit (0);
 	for (i = 0; *(str + i) != '\0'; i++)
 		*(p + i) = str[i];
 	/**(p + i + 1) = '\0';*/
