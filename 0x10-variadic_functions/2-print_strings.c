3d61d010760487232a39a08ab431fb029b1c79ca
EphremHA <ephrem.24@gmail.com>
2022-08-04 18:29:46 +0300

    print_strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * print_strings - variadic function that sums all its arguments
+ * @separator: format string
+ * @n: fixed argument
+ *
+ * Return: none
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *str;
+
+	if (n == 0)
+		exit(0);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (str != NULL)
+			printf("%s", str);
+		else if (str == NULL)
+			printf("nil");
+		if (separator != NULL && i < (n - 1))
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
