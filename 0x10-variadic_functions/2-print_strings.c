eb9de82b198605950840e9faf0ba275d83bbd7af
EphremHA <ephrem.24@gmail.com>
2022-08-04 18:33:02 +0300

    print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str != NULL)
 			printf("%s", str);
 		else if (str == NULL)
-			printf("nil");
+			printf("(nil)");
 		if (separator != NULL && i < (n - 1))
 			printf("%s", separator);
 	}
