1c9fb2017495a9c9a9ac06901bab64c98241440e
EphremHA <ephrem.24@gmail.com>
2022-08-04 18:03:26 +0300

    fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 /**
  * print_numbers - prints arguments with provided format string
@@ -16,17 +17,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	int i;
 
-	if (*separator == '0')
-		goto a;
 	va_start(ap, n);
 	for (i = 0; i < (int)n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i == (int) (n - 1))
-			goto a;
-		printf("%s", separator);
+		if (separator != NULL && i != (int) (n - 1))
+		{
+			printf("%s", separator);
+		}
 	}
-a:
 	va_end(ap);
 	printf("\n");
 }
